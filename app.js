07fb86e4c680275a0c106e33a410edc32d5db360
Carling Sugarman <csugarman@hmc.edu>
2016-08-05 10:48:39 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// create js, css, and html file
+// make sure css file is correctly hooked up (create basic div in html file and css property in css file for it)
+// install jquery make sure it's properly included
+// make sure js file is correctly hooked up (write a conosle.log in the js file and see if you can run it from the html file)
+// create text for 'player _, it's your turn'
+// css text
+// create board
+// css board
+// create a reset button that currently just console logs 'reset'
+// css previous section
+// create section for p1 wins, draws, and p2 wins
+// css previous section
+//
+// Create event listeners for each table cell, make sure they're unqiue
+// Once listeners work, add an X or O to cell on click
+// Establish 'Game' class which manages who's turn it in, and the scoreboard
+// Based on who's turn it is, mark an X or O based on click, and have it alternate
+// Create a 'determine winner' function
+// Establish detailed game parameters, not allowing clicks on already clicked areas
+// Implement reset button
